9055603b8c56779689db74b76455d9b1a44bbead
Chupriarti <wallx13@gmail.com>
2021-10-20 20:54:32 +0300

    added count state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <div className="App">
-      <h1>React App</h1>
+      <h1>React Counter</h1>
+      <h2>{count}</h2>
     </div>
   );
 }
