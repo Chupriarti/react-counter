ce5a590495bd14ec98fb8599730023833fd5a90f
Chupriarti <wallx13@gmail.com>
2021-10-21 21:32:59 +0300

    buttons in column

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h1>React Counter</h1>
       <h2>{count}</h2>
       <CounterButton callback={() => setCount(count + 1)} title="Count ++" />
+      <hr />
       <CounterButton callback={() => setCount(count - 1)} title="Count --" />
     </div>
   );
