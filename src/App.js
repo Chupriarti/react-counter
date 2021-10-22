18e25bfbf9c7ca66243f35868c0348d1952b5dd7
Chupriarti <wallx13@gmail.com>
2021-10-22 21:51:14 +0300

    added count view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css';
 import CounterButton from './components/CounterButton/CounterButton';
+import CountView from './components/CountView/CountView';
 
 function App() {
   const [count, setCount] = React.useState(0);
   return (
     <div className="App">
       <h1>React Counter</h1>
-      <h2>{count}</h2>
+      <CountView count={count} />
       <CounterButton callback={() => setCount(count + 1)} title="Count ++" />
       <hr />
       <CounterButton callback={() => setCount(count - 1)} title="Count --" />
