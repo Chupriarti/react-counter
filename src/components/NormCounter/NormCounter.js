bee0a3c0e01eaa195b42918fca5c98a6cc9b85e3
Chupriarti <wallx13@gmail.com>
2021-12-02 21:28:50 +0300

    prepared state

--- src/components/NormCounter/NormCounter.js
+++ src/components/NormCounter/NormCounter.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './normCounter.css';
 import Header from '../Header/Header';
+import CountView from '../CountView/CountView';
+
 
 export const NormCounter = () => {
+    const [a, setA] = React.useState(null);
+    const [b, setB] = React.useState(null);
+    const [currentPart, setCurrentPart] = React.useState(0);
     return (
       <div className="normCounter">
         <Header title = "React Norm Counter" />
+        <CountView count={a + b} />
       </div>
     )
 }
