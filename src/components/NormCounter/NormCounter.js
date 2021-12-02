455a5942149f0f6ee20cdb77f5d36d00ce2183b2
Chupriarti <wallx13@gmail.com>
2021-12-02 21:53:37 +0300

    prepared callbacks

--- src/components/NormCounter/NormCounter.js
+++ src/components/NormCounter/NormCounter.js
@@ -15,24 +15,29 @@ export const NormCounter = () => {
         <CountView count={a + b} />
         <table>
           <tr>
-            <td><CounterButton title="1" /></td>
-            <td><CounterButton title="2" /></td>
-            <td><CounterButton title="3" /></td>
+            <td><CounterButton onClick={() => setA(1)} title="1" /></td>
+            <td><CounterButton onClick={() => setA(2)} title="2" /></td>
+            <td><CounterButton onClick={() => setA(3)} title="3" /></td>
           </tr>
           <tr>
-            <td><CounterButton title="4" /></td>
-            <td><CounterButton title="5" /></td>
-            <td><CounterButton title="6" /></td>
+            <td><CounterButton onClick={() => setA(4)} title="4" /></td>
+            <td><CounterButton onClick={() => setA(5)} title="5" /></td>
+            <td><CounterButton onClick={() => setA(6)} title="6" /></td>
           </tr>
           <tr>
-            <td><CounterButton title="7" /></td>
-            <td><CounterButton title="8" /></td>
-            <td><CounterButton title="9" /></td>
+            <td><CounterButton onClick={() => setB(7)} title="7" /></td>
+            <td><CounterButton onClick={() => setB(8)} title="8" /></td>
+            <td><CounterButton onClick={() => setB(9)} title="9" /></td>
           </tr>
           <tr>
-            <td><CounterButton title="+" /></td>
-            <td><CounterButton title="0" /></td>
+            <td><CounterButton onClick={() => setOp("+")} title="+" /></td>
+            <td><CounterButton onClick={() => setB(0)} title="0" /></td>
             <td><CounterButton title="=" /></td>
+          </tr>
+          <tr>
+            <td><CounterButton onClick={() => setOp("-")} title="-" /></td>
+            <td><CounterButton onClick={() => setOp("/")} title="/" /></td>
+            <td><CounterButton onClick={() => setOp("*")} title="*" /></td>
           </tr>
         </table>
       </div>
