a7c4381c99a61e456246bcfe72c79e83173526b4
Chupriarti <wallx13@gmail.com>
2021-12-02 21:07:44 +0300

    moved code to SimpleCounter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import './App.css';
-import CounterButton from './components/CounterButton/CounterButton';
-import CountView from './components/CountView/CountView';
-import Header from './components/Header/Header';
+import SimpleCounter from './components/SimpleCounter/SimpleCounter';
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  
   return (
-    <div className="App">
-      <Header title = "React Counter" />
-      <CountView count={count} />
-      <CounterButton callback={() => setCount(count + 1)} title="Count ++" />
-      <CounterButton callback={() => setCount(count - 1)} title="Count --" />
-      <CounterButton callback={() => setCount(count * count)} title="Count **" />
-      <CounterButton callback={() => setCount(0)} title="Reset Count" />
-    </div>
+    <>
+      <SimpleCounter />
+    </>
   );
 }
 
