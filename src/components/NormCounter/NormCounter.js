b119072ab7d2f750f7de3293aacf428e30da2302
Chupriarti <wallx13@gmail.com>
2021-12-03 08:12:53 +0300

    prepared show result logic

--- src/components/NormCounter/NormCounter.js
+++ src/components/NormCounter/NormCounter.js
@@ -22,39 +22,60 @@ const calculate = (a, b, op) => {
 export const NormCounter = () => {
     const [a, setA] = React.useState("");
     const [b, setB] = React.useState("");
-    const [currentPart, setCurrentPart] = React.useState(0);
+    const [currentPartA, setCurrentPartA] = React.useState(true);
     const [op, setOp] = React.useState("");
 
+    const changeDigits = (digit) => {
+      if (currentPartA){
+        setA(prev => prev + digit)
+      } else {
+        setB(prev => prev + digit)
+      }
+    }
+
+    const changeOperator = (oper) => {
+      setOp(oper);
+      setCurrentPartA(prev => !prev)
+    }
+
+    const showResult = () => {
+      if (currentPartA) {
+        return a;
+      } else {
+        return b;
+      }
+    }
+    
     return (
       <div className="normCounter">
         <Header title = "React Norm Counter" />
-        <CountView count={calculate(a, b, op)} />
+        <CountView count={showResult()} />
         <table>
           <tbody>
             <tr>
-              <td><CounterButton onClick={() => setA(1)} title="1" /></td>
-              <td><CounterButton onClick={() => setA(2)} title="2" /></td>
-              <td><CounterButton onClick={() => setA(3)} title="3" /></td>
+              <td><CounterButton onClick={() => changeDigits("1")} title="1" /></td>
+              <td><CounterButton onClick={() => changeDigits("2")} title="2" /></td>
+              <td><CounterButton onClick={() => changeDigits("3")} title="3" /></td>
             </tr>
             <tr>
-              <td><CounterButton onClick={() => setA(4)} title="4" /></td>
-              <td><CounterButton onClick={() => setA(5)} title="5" /></td>
-              <td><CounterButton onClick={() => setA(6)} title="6" /></td>
+              <td><CounterButton onClick={() => changeDigits("4")} title="4" /></td>
+              <td><CounterButton onClick={() => changeDigits("5")} title="5" /></td>
+              <td><CounterButton onClick={() => changeDigits("6")} title="6" /></td>
             </tr>
             <tr>
-              <td><CounterButton onClick={() => setB(7)} title="7" /></td>
-              <td><CounterButton onClick={() => setB(8)} title="8" /></td>
-              <td><CounterButton onClick={() => setB(9)} title="9" /></td>
+              <td><CounterButton onClick={() => changeDigits("7")} title="7" /></td>
+              <td><CounterButton onClick={() => changeDigits("8")} title="8" /></td>
+              <td><CounterButton onClick={() => changeDigits("9")} title="9" /></td>
             </tr>
             <tr>
-              <td><CounterButton onClick={() => setOp("+")} title="+" /></td>
-              <td><CounterButton onClick={() => setB(0)} title="0" /></td>
+              <td><CounterButton onClick={() => changeOperator("+")} title="+" /></td>
+              <td><CounterButton onClick={() => changeDigits("0")} title="0" /></td>
               <td><CounterButton title="=" /></td>
             </tr>
             <tr>
-              <td><CounterButton onClick={() => setOp("-")} title="-" /></td>
-              <td><CounterButton onClick={() => setOp("/")} title="/" /></td>
-              <td><CounterButton onClick={() => setOp("*")} title="*" /></td>
+              <td><CounterButton onClick={() => changeOperator("-")} title="-" /></td>
+              <td><CounterButton onClick={() => changeOperator("/")} title="/" /></td>
+              <td><CounterButton onClick={() => changeOperator("*")} title="*" /></td>
             </tr>
           </tbody>
         </table>
